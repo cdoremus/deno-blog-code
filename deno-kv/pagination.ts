27ca1a8029e92e116889e5eebc1bd8944af7ed71
Craig Doremus <cdoremus@gmail.com>
2023-06-15 11:17:16 -0400

    Genericized functions in pagination

--- deno-kv/pagination.ts
+++ deno-kv/pagination.ts
@@ -2,7 +2,7 @@
  *  Demonstrates pagination using Deno KV.
  *
  * The key thing here is to use `iterator.done` to determine that you have reached the last item
- * of a list (@see printIterator).
+ * of a list (@see processIterator).
  */
 
 import { deleteAllRecords } from "./util.ts";
@@ -73,66 +73,80 @@ for (const user of users) {
 }
 
 /**
- * Obtains an iterator within a iteration series by calling Deno.Kv.list()` using a "user_by_age" prefix.
- * The start of an iteration series is signalled when the cursor value is an empty string.
+ * Obtains an iterator within a iteration series done by calling `Deno.Kv.list()` multiple times during
+ * pagination. The start of an iteration series is signalled when the cursor value is an empty string.
  *
  * @param {string} cursor - signals the start of the group you are fetching in this call. The start of
  * iteration is signalled when `cursor` is an empty string.
  * @param {number} limit - the number of items to fetch
+ * @param {Deno.KvKeyPart} - the prefix `list()` argument per iteration
  * @param {T} - the generic parameter indicating the type of the item being fetched
  * @returns {Deno.KvListIterator<T>} - iterator over items of T type
  */
 function getIterator<T>(
   cursor: string,
   limit: number,
+  prefix: Deno.KvKeyPart[],
 ): Deno.KvListIterator<T> {
   const optionsArg = cursor !== "" ? { limit, cursor } : { limit };
-  const iterator = kv.list<T>({ prefix: ["user_by_age"] }, optionsArg);
+  const iterator = kv.list<T>({ prefix }, optionsArg);
   return iterator;
 }
 
 /**
- * Prints `User` object (name and age) contained in an iterator from the call to `getIterator()`
- * to the console.
+ * Pulls out the data (items) contained in an iterator and from the call to `getIterator()`
+ * and returns them with the latest cursor string.
  *
- * @param {Deno.KvListIterator<T>} iterator - the `User` iterator
- * @param {number} pageNum - the page number being prints
- * @returns
+ * @param {T} - the type being iterated
+ * @param {Deno.KvListIterator<T>} iterator - the item iterator
+ * @returns {{ cursor: string; items: T[] }}
  */
-async function printIterator(
-  iterator: Deno.KvListIterator<User>,
-  pageNum: number,
-): Promise<{ cursor: string }> {
+async function processIterator<T>(
+  iterator: Deno.KvListIterator<T>,
+): Promise<{ cursor: string; items: T[] }> {
   let cursor = "";
   let result = await iter.next();
-  const users = [];
+  const items: T[] = [];
   while (!result.done) {
     cursor = iterator.cursor;
     // result.value returns full KvEntry object
-    const user = result.value.value as User;
-    users.push(user);
+    const item = result.value.value;
+    items.push(item as T);
     result = await iter.next();
   }
-  if (users.length > 0) {
-    console.log(`Page ${pageNum}`);
-    for (const u of users) {
-      console.log(`${u.name} ${u.age}`);
-    }
+  return { cursor, items };
+}
+
+/**
+ * Prints `User` objects (name and age) with the page number to the console.
+ *
+ * @param users - the users to be printed
+ * @param {number} pageNum - the page number to be printed
+ */
+function printUsers(users: User[], pageNum: number) {
+  console.log(`Page ${pageNum}`);
+  for (const u of users) {
+    console.log(`${u.name} ${u.age}`);
   }
-  return { cursor };
 }
 
 // print out users in batches of USERS_PER_PAGE
 const USERS_PER_PAGE = 3; // aka page size
-let pageNum = 1; // in a webapp. this will be a query param
-let cursor = ""; // in a webapp. this will be a query param
-let iter = getIterator<User>(cursor, USERS_PER_PAGE);
-await printIterator(iter, pageNum);
+const keyPart = ["user_by_age"]; // index key to sort users by age
+let pageNum = 1; // in a webapp, this will be a query param
+let cursor = ""; // in a webapp, this will be a query param
+let iter = getIterator<User>(cursor, USERS_PER_PAGE, keyPart);
+const processed = await processIterator(iter);
+printUsers(processed.items as User[], pageNum);
 pageNum++; // in a webapp. increment this in the handler
 cursor = iter.cursor; // in a webapp, reset this in the handler
 while (cursor !== "") {
-  iter = getIterator<User>(cursor, USERS_PER_PAGE);
-  const iterRet = await printIterator(iter, pageNum);
+  iter = getIterator<User>(cursor, USERS_PER_PAGE, keyPart);
+  const iterRet = await processIterator<User>(iter);
   cursor = iterRet.cursor;
+  const items: User[] = iterRet.items as User[];
+  if (items.length > 0) {
+    printUsers(items, pageNum);
+  }
   pageNum++;
 }
